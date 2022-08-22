404ce4acac987decbeddb7ce9661b5abff4d5fe4
aditya-vetale <adityavetale7028@gmail.com>
2022-08-22 23:25:35 +0530

    Functionality added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [array, setArray] = useState([]);
+  function addToArray(event) {
+    console.log("task-", task);
+    setArray([...array, task]);
+    setTask("");
+    console.log(array);
+  }
+
+  function deleteItem(id) {
+    setArray(array.filter((e, ind) => ind !== id));
+    console.log(id);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div className="input">
+        <input
+          className="addTasks"
+          placeholder="⌨ Add your tasks here... "
+          value={task}
+          onChange={(event) => {
+            setTask(event.target.value);
+          }}
+        ></input>
+        <i
+          className="fa-solid fa-plus"
+          id="plusSign"
+          onClick={addToArray}
+          title="Add item"
+        ></i>
+      </div>
+      <div className="output">
+        {array.map((ele, ind) => {
+          return (
+            <div className="List" key={ind}>
+              {ele}{" "}
+              <i
+                className="fa-solid fa-trash"
+                onClick={() => deleteItem(ind)}
+                title="Delete Task"
+              ></i>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
